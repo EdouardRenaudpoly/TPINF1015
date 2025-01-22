398a06c3cac6dec3a179f43d07f7c3e9cb7e074e
Édouard Renaud <edouard.renaud@polymtl.ca>
2025-01-22 08:27:12 -0500

    élaboration plan tp

--- TP1/EXO6/EXO6.cpp
+++ TP1/EXO6/EXO6.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+
 void GestionDictionnaire()
 {
-
+	//Smap<int, string> dictionnaire;
+	//cout << mot << "\t" << nature / genre << "\t" << définition << endl;
 }
 
 int main()
