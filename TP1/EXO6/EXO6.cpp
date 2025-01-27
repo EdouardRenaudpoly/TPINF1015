0c9dad18c78daff7bd30f13a1123fde22644b6fe
Édouard Renaud <edouard.renaud@polymtl.ca>
2025-01-27 12:56:05 -0500

    yo

--- TP1/EXO6/EXO6.cpp
+++ TP1/EXO6/EXO6.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 struct MotDictionnaire {
@@ -10,6 +12,14 @@ struct MotDictionnaire {
 void GestionDictionnaire()
 {
 	const int nMots = 4;
+	MotDictionnaire dictionnaire[nMots];
+	ifstream fichier("dictionnaire.txt");
+	string ligneActuelle;
+	int indiceLigneActuelle = 0;
+	while (getline(fichier, ligneActuelle)) { // Lit le fichier ligne par ligne
+		std::cout << ligneActuelle << std::endl;    // Affiche chaque ligne lue
+	}
+	fichier.close();
 }
 
 int main()
